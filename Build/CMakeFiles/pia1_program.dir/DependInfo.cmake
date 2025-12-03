de5a8572200dc002413523712be1c9ca3f34cd9c
Hana Mudráková <hanamu7@users.noreply.github.com>
2025-12-03 19:38:02 +0100

    Doplnenie normaly kruhu

---
+++ Build/CMakeFiles/pia1_program.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/hanamu/semestralka/pia1_kartezska_sit_a_vnorena_geometrie/KOD.cpp" "CMakeFiles/pia1_program.dir/KOD.cpp.o" "gcc" "CMakeFiles/pia1_program.dir/KOD.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
